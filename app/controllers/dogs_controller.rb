570715d87cf6143107e12ecc2c9034615097d998
Rae Farine <rfarine1027@gmail.com>
2021-07-12 13:15:32 -0400

    Allow for upload of multiple dog images

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -44,7 +44,7 @@ class DogsController < ApplicationController
   def update
     respond_to do |format|
       if @dog.update(dog_params)
-        @dog.images.attach(params[:dog][:image]) if params[:dog][:image].present?
+        @dog.images.attach(params[:dog][:images]) if params[:dog][:images].present?
 
         format.html { redirect_to @dog, notice: 'Dog was successfully updated.' }
         format.json { render :show, status: :ok, location: @dog }
@@ -73,6 +73,6 @@ class DogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def dog_params
-      params.require(:dog).permit(:name, :description, :images)
+      params.require(:dog).permit(:name, :description, images: [])
     end
 end
